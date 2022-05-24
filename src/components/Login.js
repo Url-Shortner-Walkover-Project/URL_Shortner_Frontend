19a47e56be81601ea09df28fb74dcf9d50c5a1df
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2022-05-25 02:44:25 +0530

    A small bug of routing resolved

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
+import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -110,7 +110,7 @@ const Login = (props) => {
               <Grid container>
                 <Grid item xs></Grid>
                 <Grid item>
-                  <Link href="/signup" variant="body2">
+                  <Link to="/signup">
                     {"Does'nt have an account? Sign Up"}
                   </Link>
                 </Grid>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,7 +4,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
+import {Link} from "react-router-dom"
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -128,7 +128,7 @@ const Signup = (props) => {
               <Grid container>
                 <Grid item xs></Grid>
                 <Grid item>
-                  <Link href="/login" variant="body2">
+                  <Link to="/login" >
                     {"Already have an account? Sign In"}
                   </Link>
                 </Grid>
